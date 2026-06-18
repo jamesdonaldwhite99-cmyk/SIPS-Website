62c0c043498c1e3f521181c03a351ea937a7842e
jamesdonaldwhite99-cmyk <jamesdonaldwhite99@gmail.com>
2026-06-18 12:07:42 +1000

    update

--- app/page.tsx
+++ app/page.tsx
@@ -68,7 +68,7 @@ export default function HomePage() {
 
   // Video playback rate
   useEffect(() => {
-    if (videoRef.current) videoRef.current.playbackRate = 3;
+    if (videoRef.current) videoRef.current.playbackRate = 2;
   }, []);
 
   // GSAP animations
