b5c79df5f9cdca069b0152b37254d9fd6aff7f8f
jamesdonaldwhite99-cmyk <jamesdonaldwhite99@gmail.com>
2026-07-06 15:42:41 +1000

    update

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import Script from "next/script";
 import "./globals.css";
 import Navbar from "@/components/Navbar";
 import PreFooter from "@/components/PreFooter";
@@ -26,7 +27,54 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en-AU">
+      <head>
+        {/* Google Tag Manager */}
+        <Script id="gtm" strategy="afterInteractive">
+          {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-NJ8DV8C');`}
+        </Script>
+        {/* End Google Tag Manager */}
+        {/* Meta Pixel Code */}
+        <Script id="meta-pixel" strategy="afterInteractive">
+          {`!function(f,b,e,v,n,t,s)
+{if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+n.queue=[];t=b.createElement(e);t.async=!0;
+t.src=v;s=b.getElementsByTagName(e)[0];
+s.parentNode.insertBefore(t,s)}(window, document,'script',
+'https://connect.facebook.net/en_US/fbevents.js');
+fbq('init', '1310366270331575');
+fbq('track', 'PageView');`}
+        </Script>
+        {/* End Meta Pixel Code */}
+      </head>
       <body>
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-NJ8DV8C"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          />
+        </noscript>
+        {/* End Google Tag Manager (noscript) */}
+        {/* Meta Pixel (noscript) */}
+        <noscript>
+          {/* eslint-disable-next-line @next/next/no-img-element */}
+          <img
+            height="1"
+            width="1"
+            style={{ display: "none" }}
+            src="https://www.facebook.com/tr?id=1310366270331575&ev=PageView&noscript=1"
+            alt=""
+          />
+        </noscript>
+        {/* End Meta Pixel (noscript) */}
         <Navbar />
         <main>{children}</main>
         <PreFooter />
--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,30 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
+  async redirects() {
+    return [
+      {
+        source: '/files/contact_us.php',
+        destination: '/contact',
+        statusCode: 301,
+      },
+      {
+        source: '/panelspan',
+        destination: '/products/panelspan',
+        statusCode: 301,
+      },
+      {
+        source: '/panelspan/',
+        destination: '/products/panelspan',
+        statusCode: 301,
+      },
+      {
+        source: '/files/terms-and-conditions.php',
+        destination: '/terms',
+        statusCode: 301,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
